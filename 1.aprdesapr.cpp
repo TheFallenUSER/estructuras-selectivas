e7cb763fc74d3b2cc8ad148536bd0aefb14579db
TheFallenUSER <eduardopea616@gmail.com>
2024-06-14 11:54:19 -0500

    Añadida función que valida que las notas ingresadas tengan valores comprendidos entre 0 y 100

--- 1.aprdesapr.cpp
+++ 1.aprdesapr.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 using namespace std;
 
+bool validaNota(int n){
+	while(n<0||n>100){
+		if(n<0){
+			cout<<"ERROR: Nota ingresada es menor a cero, ingrese otro valor."<<endl;
+		}else if(n>100){
+				cout<<"ERROR: Nota ingresada es mayor a 100, ingrese otro valor."<<endl;
+		}
+		cout<<"Ingrese una nota:"<<endl;
+		cin>>n;
+	}
+}
+
 int main(){
-	int n1,n2,n3,prom;
+	int nota1,nota2,nota3,prom;
 	cout<<"Ingrese la primera nota:"<<endl;
-	cin>>n1;
+	cin>>nota1;
+	validaNota(nota1);
 	cout<<"Ingrese la segunda nota:"<<endl;
-	cin>>n2;
+	cin>>nota2;
+	validaNota(nota2);
 	cout<<"Ingrese la tercera nota:"<<endl;
-	cin>>n3;
-	prom=(n1+n2+n3)/3;
+	cin>>nota3;
+	validaNota(nota3);
+	prom=(nota1+nota2+nota3)/3;
 	if(prom>=70){
 		cout<<"Felicidades, aprobaste."<<endl;
 	}
